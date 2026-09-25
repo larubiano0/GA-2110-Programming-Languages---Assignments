df15396486be6f17ca47f7a98df3863412366276
Luis Alejandro Rubiano <larubiano0@gmail.com>
2026-09-25 13:53:44 -0400

    Finished ps1.rkt

--- pset1-release/ps1.rkt
+++ pset1-release/ps1.rkt
@@ -34,8 +34,63 @@
 ; Example: when s is "The gig was a gag, a joke.", (palindrome-list s) should return
 ; '("gig" "a" "gag" "a").
 
-(define (palindrome-list s) '())
-; accumulator of indicators using helper func
+(define (plain-is-palindrome? word)
+  (cond
+    [(= (string-length word) 0) #t]
+    [(= (string-length word) 1) #t]
+    [else (and (plain-is-palindrome? (substring word 1 (- (string-length word) 1))) (char=? (string-ref word 0) (string-ref word (- (string-length word) 1))))]))
+
+(define (is-in? char string) ;receives char as one string of one char for example "a"
+(cond
+ [(eq? (string-length string) 0) #f]
+ [(is-in? char (substring string 1)) 1]
+ [(eq? (string-ref char 0) (string-ref string 0)) 1]
+ [else #f]
+))
+
+(define (to-lowercase-remove-punctuation s1)
+    (cond
+    [(eq? (string-length s1) 0) s1]
+    [(is-in? (string (string-ref s1 0)) " abcdefghijklmnopqrstuvwxyz") (string-append (string (string-ref s1 0)) (to-lowercase-remove-punctuation (substring s1 1))) ]
+    [(string=? (string (string-ref s1 0)) "A") (string-append "a" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "B") (string-append "b" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "C") (string-append "c" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "D") (string-append "d" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "E") (string-append "e" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "F") (string-append "f" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "G") (string-append "g" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "H") (string-append "h" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "I") (string-append "i" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "J") (string-append "j" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "K") (string-append "k" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "L") (string-append "l" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "M") (string-append "m" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "N") (string-append "n" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "O") (string-append "o" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "P") (string-append "p" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "Q") (string-append "q" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "R") (string-append "r" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "S") (string-append "s" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "T") (string-append "t" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "U") (string-append "u" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "V") (string-append "v" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "W") (string-append "w" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "X") (string-append "x" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "Y") (string-append "y" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [(string=? (string (string-ref s1 0)) "Z") (string-append "z" (to-lowercase-remove-punctuation (substring s1 1)))]
+    [else (to-lowercase-remove-punctuation (substring s1 1))]
+    ))
+
+(define (check-palindrome-list list)
+  (cond
+    [(null? list) '()]
+    [(plain-is-palindrome? (first list)) (cons (first list) (check-palindrome-list (rest list)))]
+    [else (check-palindrome-list (rest list))]))
+
+(define (palindrome-list s)
+  (check-palindrome-list (string-split (to-lowercase-remove-punctuation s)))
+  )
+
 
 
 ;;;;;;;;; Problem 2 ;;;;;;;;;
@@ -60,7 +115,12 @@
 (struct times-node (arg1 arg2))
 (struct int-leaf (val))
 
-(define (eval-tree t) '())
+(define (eval-tree t)  
+  (cond
+    [(int-leaf? t) (int-leaf-val t)]
+    [(plus-node? t) (+ (eval-tree (plus-node-arg1 t)) (eval-tree (plus-node-arg2 t)))]
+    [(times-node? t) (* (eval-tree (times-node-arg1 t)) (eval-tree (times-node-arg2 t)))]
+   ))
 
 ;;;;;;;;; Problem 3 ;;;;;;;;;;;;
 
@@ -87,7 +147,32 @@
 (struct bt-leaf (val))
 (struct bt-empty ())
 
-(define (check-bt t) '())
+(define (get-highest-left t)
+  (cond
+    [(bt-empty? t) -inf.0]
+    [(bt-leaf? t) (bt-leaf-val t)]
+    [else (max (bt-node-val t) (get-highest-left (bt-node-left t)) (get-highest-left (bt-node-right t)))]
+    ))
+
+(define (get-lowest-right t)
+  (cond
+    [(bt-empty? t) +inf.0]
+    [(bt-leaf? t) (bt-leaf-val t)]
+    [else (min (bt-node-val t) (get-lowest-right (bt-node-left t)) (get-lowest-right (bt-node-right t)))]
+    ))
+
+
+(define (check-bt t)
+  (cond
+    [(bt-empty? t) #t]
+    [(bt-leaf? t) #t]
+    [else
+     (and
+      (< (get-highest-left (bt-node-left t))(bt-node-val t))
+      (> (get-lowest-right (bt-node-right t))(bt-node-val t))
+      (check-bt (bt-node-left t))
+      (check-bt (bt-node-right t)))]
+    ))
 
 ;;;;;;;;; Problem 4 ;;;;;;;;;;;;
 
@@ -112,7 +197,23 @@
 ; 1)) would be wrong because we should prefer to make the first list
 ; larger at the expense of making the second list shorter.
 
-(define (ml-split l) '())
+(define (ml-split-helper remaining current direction)
+    (cond
+      [(empty? remaining) (list (reverse current))]
+      [else
+         (cond
+           [(eq? direction 'unknown-direction)
+            (cond
+              [(> (first remaining) (first current)) (ml-split-helper (rest remaining) (cons (first remaining) current) 'increasing)]
+              [(< (first remaining) (first current)) (ml-split-helper (rest remaining) (cons (first remaining) current) 'decreasing)]
+              [else (cons (reverse current) (ml-split-helper (rest remaining) (list (first remaining)) 'unknown-direction))])]
+           [(eq? direction 'increasing) (if (> (first remaining) (first current)) (ml-split-helper (rest remaining) (cons (first remaining) current) 'increasing) (cons (reverse current) (ml-split-helper (rest remaining) (list (first remaining)) 'unknown-direction)))]
+           [else (if (< (first remaining) (first current)) (ml-split-helper (rest remaining) (cons (first remaining) current) 'decreasing) (cons (reverse current) (ml-split-helper (rest remaining) (list (first remaining)) 'unknown-direction)))])]))
+
+(define (ml-split l)
+  (cond
+    [(empty? l) '()]
+    [else (ml-split-helper (rest l) (list (first l)) 'unknown-direction)]))
 
 ;;;;;;;;; Problem 5 ;;;;;;;;;;;;
 
@@ -132,4 +233,8 @@
 ; Example: (piles-insert '((4) (5)) 3) should return '((3 4) (5))) and
 ; (piles-insert '((2) (6)) 4) should return '((2) (4 6)))
 
-(define (piles-insert ls n) '())
+(define (piles-insert ls n)
+  (cond
+    [(empty? ls) (list (list n))]
+    [(>= (first (first ls)) n) (cons (cons n (first ls)) (rest ls))]
+    [else (cons (first ls) (piles-insert (rest ls) n))]))
